670dc1341c1ee729d5c7e998f5dc9f19641f70a0
billysco <70048321+billysco@users.noreply.github.com>
2021-05-16 16:02:44 -0700

    Added minor changes

--- joint_savings.sol
+++ joint_savings.sol
@@ -56,13 +56,13 @@ contract JointSavings {
         }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        recipient.transfer(amount)
+        recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        lastWithdrawAmount = amount
+        lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
-        contractBalance = address(this).balance
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
@@ -71,7 +71,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        contractBalance = address(this).balance
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -80,12 +80,12 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
     Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {}
 }
-
